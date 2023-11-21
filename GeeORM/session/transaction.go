0bbce65b8a7c75368eae38cc1c5f5e106a789bee
KNICEX <1840586637@qq.com>
2023-11-22 00:47:13 +0800

    update geeorm

---
+++ GeeORM/session/transaction.go
@@ -0,0 +1,30 @@
+package session
+
+import "geeorm/logger"
+
+func (s *Session) Begin() (err error) {
+	logger.Info("transaction begin")
+	if s.tx, err = s.db.Begin(); err != nil {
+		logger.Error(err)
+		return
+	}
+	return
+}
+
+func (s *Session) Commit() (err error) {
+	logger.Info("transaction commit")
+	if err = s.tx.Commit(); err != nil {
+		logger.Error(err)
+		return
+	}
+	return
+}
+
+func (s *Session) Rollback() (err error) {
+	logger.Info("transaction rollback")
+	if err = s.tx.Rollback(); err != nil {
+		logger.Error(err)
+		return
+	}
+	return
+}
